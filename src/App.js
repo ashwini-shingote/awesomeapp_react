8f88802df860a898a0a6a245c7312f7e95a36617
ashwini-shingote <ashwini.dhawale@gmail.com>
2024-01-16 21:31:52 +0100

    component instances with necessaryprop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Some Simple Title" />
+        <Pokemon
+          name="Charizard"
+          weight={90}
+          awesome={true}
+          terrifying={false}
+          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight={6.9}
+          awesome={true}
+          terrifying={false}
+          abilities={["Overgrow", "Chlorophyll"]}
+        />
+        <Pokemon
+          name="Mewtwo"
+          weight={122}
+          awesome={true}
+          terrifying={true}
+          abilities={["Pressure", "Unnerve", "Steadfast", "Insomnia"]}
+        />
+        <Pokemon
+          name="Mega Beedrill"
+          weight={65}
+          awesome={false}
+          terrifying={true}
+          abilities={["Intimidate", "Unnerve"]}
+        />
       </main>
       <h1>Hello World!</h1>
       <p>React</p>
